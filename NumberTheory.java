9cc97cf07283ffb534d5190807fcc0745b2192f3
Josh Humpherys <joshua.humpherys@gmail.com>
2017-02-22 17:20:33 -0500

    Implement extended GCD

--- NumberTheory.java
+++ NumberTheory.java
@@ -5,6 +5,8 @@
 
 // Extended GCD, Gauss' Algorithm for CRT, and modular exponentiation
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NumberTheory {
@@ -18,7 +20,10 @@ public class NumberTheory {
         if(algorithm == 1) {
             int a = prompt("a: ", s);
             int b = prompt("b: ", s);
-            extendedGCD(a, b).printTable();
+            ExtendedGCD extendedGCD = extendedGCD(a, b);
+            extendedGCD.printTable();
+            extendedGCD.printResult();
+            extendedGCD.printCheck();
         } else if(algorithm == 2) {
             int n = prompt("Number of congruences: ", 1, s);
             int[] aValues = new int[n];
@@ -69,7 +74,9 @@ public class NumberTheory {
     }
 
     private static ExtendedGCD extendedGCD(int a, int b) {
-        return new ExtendedGCD(a, b);
+        ExtendedGCD extendedGCD = new ExtendedGCD(a, b);
+        extendedGCD.run();
+        return extendedGCD;
     }
 
     private static int gauss(int[] aValues, int[] nValues) {
@@ -81,12 +88,143 @@ public class NumberTheory {
     }
 
     private static class ExtendedGCD {
+        int Q_INDEX = 0;
+        int R_INDEX = 1;
+        int X_INDEX = 2;
+        int Y_INDEX = 3;
+        int A_INDEX = 4;
+        int B_INDEX = 5;
+        int X2_INDEX = 6;
+        int X1_INDEX = 7;
+        int Y2_INDEX = 8;
+        int Y1_INDEX = 9;
+        List<int[]> rows;
+        int a;
+        int b;
+        int gcd;
+        int x;
+        int y;
         ExtendedGCD(int a, int b) {
+            rows = new ArrayList<>();
+            if(Math.abs(a) < Math.abs(b)) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            this.a = a;
+            this.b = b;
+            rows.add(new int[]{0, 0, 0, 0, a, b, 1, 0, 0, 1});
+        }
 
+        void run() {
+            while(!finished()) {
+                int[] lastRow = getLastRow();
+                int[] newRow = new int[10];
+                newRow[Q_INDEX] = lastRow[A_INDEX] / lastRow[B_INDEX];
+                newRow[R_INDEX] = lastRow[A_INDEX] % lastRow[B_INDEX];
+                newRow[X_INDEX] = lastRow[X2_INDEX] - newRow[Q_INDEX] * lastRow[X1_INDEX];
+                newRow[Y_INDEX] = lastRow[Y2_INDEX] - newRow[Q_INDEX] * lastRow[Y1_INDEX];
+                newRow[A_INDEX] = lastRow[B_INDEX];
+                newRow[B_INDEX] = newRow[R_INDEX];
+                newRow[X2_INDEX] = lastRow[X1_INDEX];
+                newRow[X1_INDEX] = newRow[X_INDEX];
+                newRow[Y2_INDEX] = lastRow[Y1_INDEX];
+                newRow[Y1_INDEX] = newRow[Y_INDEX];
+                rows.add(newRow);
+            }
+            int[] lastRow = getLastRow();
+            gcd = lastRow[A_INDEX];
+            x = lastRow[X2_INDEX];
+            y = lastRow[Y2_INDEX];
+            if(gcd < 0) {
+                gcd *= -1;
+                x *= -1;
+                y *= -1;
+            }
+        }
+
+        private boolean finished() {
+            int[] lastRow = getLastRow();
+            return lastRow[Q_INDEX] != 0 && lastRow[R_INDEX] == 0;
+        }
+
+        private int[] getLastRow() {
+            return rows.get(rows.size() - 1);
         }
 
         void printTable() {
+            int[] maxWidthOfColumns = new int[10];
+            for(int i = 0; i < 10; i++) {
+                int maxWidth = 0;
+                for(int j = 0; j < rows.size(); j++) {
+                    int[] currentRow = rows.get(j);
+                    int currentWidth = String.valueOf(rows.get(j)[i]).length();
+                    if(currentWidth > maxWidth) {
+                        maxWidth = currentWidth;
+                    }
+                }
+                maxWidthOfColumns[i] = maxWidth;
+            }
+            String[] spaces = new String[12];
+            spaces[0] = "";
+            for(int i = 1; i < 12; i++) {
+                spaces[i] = spaces[i - 1] + " ";
+            }
+            StringBuilder header = new StringBuilder();
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[0] - 1, 0)) + "q |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[1] - 1, 0)) + "r |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[2] - 1, 0)) + "x |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[3] - 1, 0)) + "y |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[4] - 1, 0)) + "a |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[5] - 1, 0)) + "b |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[6] - 2, 0)) + "x2 |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[7] - 2, 0)) + "x1 |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[8] - 2, 0)) + "y2 |");
+            header.append(" " + getNumSpaces(spaces, Math.max(maxWidthOfColumns[9] - 2, 0)) + "y1");
+            System.out.println(header);
+            StringBuilder dashes = new StringBuilder();
+            for(int i = 0; i < header.length(); i++) {
+                dashes.append("-");
+            }
+            System.out.println(dashes);
+            StringBuilder headerFormatBuilder = new StringBuilder();
+            for(int i = 0; i < 10; i++) {
+                headerFormatBuilder.append(" %" + maxWidthOfColumns[i] + "d " + (i != 9 ? "|" : ""));
+            }
+            headerFormatBuilder.append("%n");
+            String headerFormat = headerFormatBuilder.toString();
+            for(int i = 0; i < rows.size(); i++) {
+                int[] row = rows.get(i);
+                System.out.printf(headerFormat, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]);
+            }
+        }
 
+        private String getNumSpaces(String[] spaces, int n) {
+            StringBuilder space = new StringBuilder(spaces[Math.min(n, spaces.length - 1)]);
+            for(int i = spaces.length; i <= n; i++) {
+                space.append(" ");
+            }
+            return space.toString();
+        }
+
+        void printResult() {
+            System.out.println("gcd(" + a + ", " + b + ") = " + gcd);
+        }
+
+        void printCheck() {
+            System.out.println("(" + a + ")" + "(" + x + ") + (" + b + ")(" + y + ") = " + gcd);
+        }
+
+        int getGCD() {
+            return gcd;
+        }
+
+        int getX() {
+            return x;
+        }
+
+        int getY() {
+            return y;
         }
     }
 }
