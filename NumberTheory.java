2087b9dbba6d1a4c5ed129dc3c854bdcbd8c32ef
Josh Humpherys <joshua.humpherys@gmail.com>
2017-02-23 10:08:18 -0500

    Implement modular exponentiation

--- NumberTheory.java
+++ NumberTheory.java
@@ -37,7 +37,9 @@ public class NumberTheory {
             int a = prompt("a: ", s);
             int x = prompt("x: ", 0, s);
             int n = prompt("n: ", 0, s);
-            System.out.println(modularExponentiation(a, x, n));
+            ModularExponentiation modularExponentiation = modularExponentiation(a, x, n);
+            modularExponentiation.printTable();
+            modularExponentiation.printResult();
         }
     }
 
@@ -89,8 +91,10 @@ public class NumberTheory {
         return gauss;
     }
 
-    private static int modularExponentiation(int a, int x, int n) {
-        return 0;
+    private static ModularExponentiation modularExponentiation(int a, int x, int n) {
+        ModularExponentiation modularExponentiation = new ModularExponentiation(a, x, n);
+        modularExponentiation.run();
+        return modularExponentiation;
     }
 
     private static class ExtendedGCD extends TabularAlgorithm {
@@ -245,6 +249,67 @@ public class NumberTheory {
                 product *= nValues[i];
             }
             result = mod(sum, product);
+        }
+
+        @Override
+        protected String[] getLabels() {
+            return labels;
+        }
+
+        @Override
+        protected List<int[]> getRows() {
+            return rows;
+        }
+    }
+
+    private static class ModularExponentiation extends TabularAlgorithm {
+        int a;
+        int x;
+        int n;
+        boolean[] binaryArray;
+        List<int[]> rows;
+        int result;
+        String[] labels = new String[]{"i", "a^i(mod n)", "x"};
+        ModularExponentiation(int a, int x, int n) {
+            rows = new ArrayList<>();
+            this.a = a;
+            this.x = x;
+            this.n = n;
+        }
+
+        void run() {
+            boolean[] binaryArray = convertDecimalToBinary(x);
+            this.binaryArray = binaryArray;
+            int powerOfTwo = 1;
+            int aToPowerOfTwoModN = mod(a, n);
+            int productModN = binaryArray[0] ? aToPowerOfTwoModN : 1;
+            rows.add(new int[]{powerOfTwo, aToPowerOfTwoModN, binaryArray[0] ? 1 : 0});
+            for(int i = 1; i < binaryArray.length; i++) {
+                powerOfTwo = powerOfTwo << 1;
+                aToPowerOfTwoModN = mod((int)Math.pow(aToPowerOfTwoModN, 2), n);
+                if(binaryArray[i]) {
+                    productModN = mod(productModN * aToPowerOfTwoModN, n);
+                }
+                rows.add(new int[]{powerOfTwo, aToPowerOfTwoModN, binaryArray[i] ? 1 : 0});
+            }
+            result = productModN;
+        }
+
+        private static boolean[] convertDecimalToBinary(int x) {
+            String binaryString = Integer.toBinaryString(x);
+            boolean[] binaryArray = new boolean[binaryString.length()];
+            for(int i = 0; i < binaryString.length(); i++) {
+                binaryArray[binaryArray.length - 1 - i] = binaryString.charAt(i) == '1';
+            }
+            return binaryArray;
+        }
+
+        void printResult() {
+            System.out.println(addParensWhenNegative(a) + "^" + addParensWhenNegative(x) + "(mod " + n + ")\u2261 " + result);
+        }
+
+        private String addParensWhenNegative(int i) {
+            return i < 0 ? "(" + i + ")" : String.valueOf(i);
         }
 
         @Override
